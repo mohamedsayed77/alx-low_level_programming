135a89a115cbd76661ed6a53cfde36452b7fd879
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-07 19:25:03 +0300

    hello wolrd

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
-#include
+#include <stdio.h>
 /**
 * main - Entry poit
-* Description: A C program that use typeof operation of various types.
+* Description: A C program that use sizeof operation of various types.
 * Return: Always 0 (success)
 */
 int main(void)
 {
-	printf("size of a char: %u byte(s)\n", typeof(char));
-	printf("size of an int: %u byte(s)\n", typeof(int));
-	printf("size of a long int: %u byte(s)\n", typeof(long int));
-	printf("size of a long long int: %u byte(s)\n", typeof(long long int));
-	printf("size of a float: %u byte(s)\n", typeof(float));
+	printf("size of a char: %u byte(s)\n", sizeof(char));
+	printf("size of an int: %u byte(s)\n", sizeof(int));
+	printf("size of a long int: %u byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %u byte(s)\n", sizeof(long long int));
+	printf("size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
