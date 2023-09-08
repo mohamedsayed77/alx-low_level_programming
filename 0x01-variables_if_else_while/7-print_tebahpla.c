a1c5b67f6da8fe1e61f26c6c555e44ab392937a0
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-09 00:10:37 +0300

    added readme file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a program that print alphabatec in reverse
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+
+	char ch = 'z'
+
+	while (ch >= 'a')
+	{
+
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n\);
+
+	return (0);
+}
