7ae065b582fd7b3a153ad97244504e36362b1787
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-09 00:21:18 +0300

    added readme file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description:  a program that prints of base 16 with putchar
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+
+	int digit = 48;
+
+	while (digit <= 102)
+	{
+
+		putchar(digit);
+
+		if (digit == 57)
+		digit += 39;
+			digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
