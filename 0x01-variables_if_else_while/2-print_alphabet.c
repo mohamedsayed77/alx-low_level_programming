f6fa91c3d39a9a6ce5859e9642710c8391ccd7be
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-08 22:11:41 +0300

    added readme file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ * main - Entry pint
+ * Description: a
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	outchar('\n');
+
+	return (0);
+}
