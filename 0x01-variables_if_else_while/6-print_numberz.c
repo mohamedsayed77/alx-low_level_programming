6fad3ac633193aca88ef47150dff87633752fde9
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-09 00:04:01 +0300

    added readme file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry pint
+ * Description: a program that prints all single digit numbers of base
+ * 10 with putchar
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+
+		putchar(digit + '0');
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
