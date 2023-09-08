c1a2c7019d291d86055121f18deec7ed049693ad
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-09 01:33:58 +0300

    added readme file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a
+ * Return: 0 (success)
+*/
+
+int main(void)
+{
+
+	int firstDigit = 0, secondDigit;
+
+	while (firstDigit <= 99)
+	{
+
+		seondDigit = firstDigit;
+		while (secondDigit <= 99)
+		{
+
+			if (secondDigit != firstDigit)
+			{
+
+				putchar((firstDigit / 10) + 48);
+				putchar((firstDigit / 10) + 48);
+				putchar(' ');
+				putchar((secondDigit / 10) + 48);
+				putchar((secondDigit / 10) + 48);
+
+				if (firstDigit != 98 || secondDigit != 99)
+				{
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			secondDigit++;
+		}
+		firstDigit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
