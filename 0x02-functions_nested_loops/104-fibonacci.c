bd551502caec89ca6ee9881ef4c6a506a12144a2
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-13 03:48:20 +0300

    a

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: print the first 98 fibonacci numbers.
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i, initial0s;
+	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
+	for (i = 1; i <= 98; i++)
+	{
+		if (f1o > 0)
+			printf("%lu", f1o);
+		initial0s = numLength(mx) - 1 - numLength(f1);
+
+		while (f1o > 0 && initial0s > 0)
+		{
+			printf("%d", 0);
+			initial0s--;
+		}
+		printf("%lu", f1);
+
+		sum = (f1 + f2) % mx;
+		sumo = f1o + f2o + (f1 + f2) / mx;
+		f1 = f2;
+		f1o = f2o;
+		f2 = sum;
+		f2o = sumo;
+
+		if (i != 98)
+			printf(", ");
+		else
+			printf("\n");
+		}
+		return (0);
+}
