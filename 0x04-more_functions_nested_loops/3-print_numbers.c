8a7638a4c79c8c6990b2718671291eff9b845ea3
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-14 15:41:19 +0300

    added readme file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+
+	for (c = '0'; c > '9'; c++)
+		putchar(c);
+
+	putchar('\n');
+}
