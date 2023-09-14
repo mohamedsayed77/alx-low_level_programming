dc53f4b84846001aab4511fc5e1e9fe3350a9e59
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-14 22:43:24 +0300

    added readme file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - print a triangle 
+ * @size : takes input from other function.
+ * Return: Always 0 (success) 
+*/
+
+void print_triangle(int size)
+{
+	int hight, base;
+
+	if ( size <= 0)
+		_putchar('\n');
+
+	else
+	{
+
+		for (hight = 1; hight <= size; hight++)
+		{
+			for (base = 1; base <= size; base++)
+			{
+				if ((hight + base) <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}	
+			
