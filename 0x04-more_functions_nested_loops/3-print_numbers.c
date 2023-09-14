d27a2746ba10c0a5e19e76ee44d583c219958926
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-14 15:48:45 +0300

    added readme file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,6 @@
 
 /**
  * print_numbers - a function that prints the numbers
- * Return: always 0 (success)
 */
 
 void print_numbers(void)
@@ -10,10 +9,9 @@ void print_numbers(void)
 	char c;
 
 
-	for (c = '0'; c > '9'; c++)
+	for (c = '0'; c < '9'; c++)
 		putchar(c);
 
 	putchar('\n');
 
-	return (0);
 }
