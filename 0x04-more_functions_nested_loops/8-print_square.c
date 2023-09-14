f6df28d130edb86531aa36d4495e1fa8d4b5139d
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-14 20:54:01 +0300

    added readme file

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,8 +11,8 @@ void print_square(int size)
 
 	for (row = 1; row <= size; row++)
 	{
-		for (column = 1; column < size; column++)
-			_putchar('#');
+		for (column = 1; column <= size; column++)
+			_putchar(35);
 		_putchar('\n');
 	}
 }
