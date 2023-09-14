54651353d3c75bddccace641694a419a856dd066
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-14 16:00:16 +0300

    added readme file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ * except 2 and 4
+*/
+
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+
+		if (c == 2 && c == 4)
+		{
+			c++;
+		}
+	putchar('\n');
+}
