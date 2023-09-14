fa14f65259fe0990f5352a9cf52c08efd77dc4a5
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-14 22:20:21 +0300

    added readme file

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,14 +8,16 @@ int main(void)
 {
 	for (int num = 1; num <= 100; num++)
 	{
-		if (num % 3 == 0 && num % 5 == 0)
-			printf("FizzBuzz");
-		else if (num % 3 == 0)
-			printf("Fizz:);
-		else if (num % 5 == 0)
+		if (num % 3 == 0 && (num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
 			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
 
-		if (num < 100)
+		if (num != 100)
 			printf(" ");
 		else
 			printf("\n");
