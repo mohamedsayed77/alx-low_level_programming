f5ed5dfc4a9a4971677547e9c7d311dac39e2f66
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-18 23:47:12 +0300

    pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - a function that prints a string
+ *
+ * @str: take address of a string variable or array
+ *
+ *
+*/
+
+void _puts(char *str)
+{
+
+	puts(str);
+}
