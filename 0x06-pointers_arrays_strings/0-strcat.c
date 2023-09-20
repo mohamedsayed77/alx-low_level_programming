7c235f0c838b1fe69f73e20f01f6697062ef6704
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-20 20:16:04 +0300

    str func

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * *_strcat - a function that concatenates two strings.
+ * *_strcat - a function that concatenates two strings
  *
- * @dest: take a string 
- 8 @src: take a string
+ * @dest : pointer to destnation input
+ * @src : pointer to source input
  *
  * Return: dest concatenated to src
 */
@@ -12,5 +12,5 @@
 char *_strcat(char *dest, char *src)
 {
 	strcat(dest, src);
-	return dest;
+	return (dest);
 }
