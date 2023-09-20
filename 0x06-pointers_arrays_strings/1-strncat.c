40cc41adee35668858d721712b4a5871a4bf684a
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-20 20:53:53 +0300

    str func

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * *_strncat - a function that concatenates two strings
+ *
+ * @dest : pointer to destnation input
+ * @src : pointer to source input
+ * @n: specify the num of characers that will concatnate
+ * Return: dest concatenated to src by specifying the num of characters of src
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncar(dest, src, n)
+	return (dest);
+}
