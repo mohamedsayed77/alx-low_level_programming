d0ef4ae616ee70bbce24d150d184974f41f944e8
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-21 00:43:41 +0300

    str func

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,6 +11,16 @@
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
+
+	int c, c2;
+
+	c = 0;
+
+	while (dest[c])
+		c++;
+
+	for (c2 = 0; src[c2]; c2++;
+		dest[c++] = src[c2];
 	return (dest);
+
 }
