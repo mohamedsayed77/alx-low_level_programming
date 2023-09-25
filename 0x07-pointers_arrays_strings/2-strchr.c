84e5fb061bd09ac99bfcf17b93bd96dbc188f3a1
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-09-25 23:52:10 +0300

    pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,16 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
 
-	if (c == '\0')
-	{
-		return (s);
-	}
+	int i;
 
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	return ('\0');
 }
