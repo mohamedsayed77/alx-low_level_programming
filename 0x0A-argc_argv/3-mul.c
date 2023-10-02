ce68a43f0ea84f7de5868c2d22116f96e8d37a61
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-03 02:30:17 +0300

    main arg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,15 +14,18 @@ int main(int argc, char *argv[])
 
 	int sum;
 
-	if (argc == 3)
-	{
-		sum = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", sum);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	return (0)
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int result = num1 * num2;
+
+	printf("%d\n", result);
+
+	return (0);
 }
+
