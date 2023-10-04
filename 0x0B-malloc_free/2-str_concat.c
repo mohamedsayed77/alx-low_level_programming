7bd4754eab8b662ec13c451f1644508ca2ed523a
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-04 20:35:26 +0300

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,35 +1,57 @@
 #include "main.h"
 
 /**
+ * _strlen - find len of the string
+ * @s : pointer to string
+ * return: int
+*/
+
+int _strlen(char *s)
+{
+
+	int len = 0;
+
+	for (; s[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
  * *str_concat - a function that concatnate 2 string and return them
  * to allocation space
  * @s1: pointer to string
  * @s2 : pointer to string
  * Return: return concatnated string to allocation space
 */
-
 char *str_concat(char *s1, char *s2)
 {
-	int len1, len2;
+
+	int len1, len2, i;
 	char *cat;
 
-	if (s1 == NULL)
-		s1 = '\0';
 
+	if (s1 == NULL)
+		s1 = "\0";
 
 	if (s2 == NULL)
-		s2 = '\0';
+		s2 = "\0";
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	cat = malloc((len1 + len2) * sizeof(char) + 1);
 
-	cat = (char *)  malloc(len1 + len2 + 1);
+	if (cat == 0)
+		return (0);
 
-	if (cat == NULL)
-		return (NULL);
+	for (i = 0; i <= len1 + len2; i++)
+	{
+		if (i < len1)
+			cat[i] = s1[i];
 
-	strcpy(cat, s1);
-	strcat(cat, s2);
-
+	else
+			cat[i] = s2[i - len1];
+	}
+	cat[i] = '\0';
 	return (cat);
 }
