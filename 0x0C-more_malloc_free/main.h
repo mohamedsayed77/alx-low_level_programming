05c9c80bf99d0c47b5c885231c4bb84c31cc8105
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-09 02:56:34 +0300

    more malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -16,7 +16,9 @@ int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _atoi(const char *s);
 void print_int(unsigned long int n);
-
+int is_digit(char *s);
+void error(void);
+int _strlen(char *s);
 
 
 
