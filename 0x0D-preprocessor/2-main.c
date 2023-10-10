af4f86aa81048309c1f8ba4046b325ea5817dffa
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-10 15:05:06 +0300

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - start point to a program that print name of current file
+ * Return: 0 means success
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
