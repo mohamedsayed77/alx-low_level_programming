0eaa081d83c284ec2cfdd79d4ce06412d840bb00
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-13 01:01:40 +0300

    varidac function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - a function that print numbers separated by string
+ * print_numbers - a function that print numbers separated by string ,
  * @separator : string that will be printed between numbers
  * @n : number of arguments
  * Return: void
@@ -10,16 +10,19 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
-	unsigned int i;
+	int i = n;
 
+
+	if (!n)
+	{
+		printf("\n");
+		return;
+	}
 	va_start(args, n);
 
-	for (i = 0; i < n; i++)
-	{
-		printf("%d", va_arg(args, int));
-		if (i < n - 1 && separator != NULL)
-			printf("%s", separator);
-	}
-	va_end(args)
-	printf("\n");
+	while (i--)
+		printf("%d%s", va_arg(args, int),
+		i ? (separator ? separator : "") : "\n");
+
+	va_end(args);
 }
