f243b5a0ed39893f47b5547f9d1e1b51caca5844
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-20 01:31:56 +0300

    liked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node - a function that adds a node to the head of the list
+ * @head: the address of the pointer to head
+ * @str: str field of the node
+ * return: the size of the list
+ *
+ * Description: singly linked list node structure
+ */
+
+
+list_t *add_node(list_t **head, const char *str);
+{
+
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (!head || !new_node)
+		return (null);
+
+	if (str)
+	{
+		new_node->str = strdup(str);
+
+		if (!new_node->str)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		new_node->len = _strlen(new_node->str);
+	}
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
