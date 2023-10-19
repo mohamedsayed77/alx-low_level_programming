025eda7660a5c665f35dfc65a2ab9e8da8c424d8
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-20 01:12:36 +0300

    liked lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - a function that determines the length of linked list
+ * @h: pointer to the head
+ * Return: the size of list
+ *
+ */
+
+size_ list_len(const list_t *h)
+{
+
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i)
+}
