8f8fe98be8032a75678f1289ae0803de3a9b067c
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-20 02:36:36 +0300

    liked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-void _constructor(void) __attribute__ (constructor));
+void _constructor(void) __attribute__ ((constructor));
 
 
 /**
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,6 +15,8 @@
  *
  * Description: singly linked list node structure
  */
+
+
 typedef struct list_s
 {
 	char *str;
@@ -29,7 +31,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 
 
 
