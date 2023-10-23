4a426137864f2ab8e144bcf837541143c7793ac1
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-23 19:30:35 +0300

    more linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (node)
 	{
 
-		if (i == idx - 1)
+		if (count == idx - 1)
 		{
 
 			new_node->next = node->next;
