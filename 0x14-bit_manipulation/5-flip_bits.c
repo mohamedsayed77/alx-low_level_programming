59f2342efa769378907aa9d93c7623a420c84d9f
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2023-10-26 23:10:23 +0200

    bitwise manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 
-	unsinged long int xorvalue = n ^ m;
+	unsigned long int xorvalue = n ^ m;
 	unsigned int count = 0;
 
 	while (xorvalue)
