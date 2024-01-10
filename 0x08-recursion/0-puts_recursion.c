6fb8ed48542d6a23c2f6f177936f120435c0f3d4
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-01-10 23:07:10 +0200

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,7 +2,7 @@
 /**
  * _puts_recursion - a function that call itself and print a string
  *
- * @s: take a string that will be printed
+ * @s : take a string that will be printed
  */
 
 
@@ -11,7 +11,6 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
-		return;
 	}
 	else
 	{
