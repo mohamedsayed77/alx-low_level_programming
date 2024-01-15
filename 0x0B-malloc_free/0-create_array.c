4e04d0035f17728b8a3eba104f6b2e0e8fd1bd43
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-01-15 18:04:29 +0200

    allocate

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,4 @@
-#include "main.h
+#include "main.h"
 
 /**
 * create_array - a function that create array of char
@@ -11,12 +11,13 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
+	char *arr;
 
 
 	if (size == 0)
 		return (NULL);
 
-	char *arr = malloc(sizeof(char) * size);
+	arr = malloc(sizeof(char) * size);
 
 	if (arr == NULL)
 		return (NULL);
