4f6aa16639f9ff769c21931e829db7faca5082fb
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-01-18 21:02:04 +0200

    more allocate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		arr[i] = s1[i];
 
 	for (j = 0; j < n; j++)
-		arr[i++] = s2[j];
+	{
+		arr[i] = s2[j];
+		i++;
+	}
 
 	arr[i] = '\0';
 
