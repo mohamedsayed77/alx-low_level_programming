f09b5e02be32d7320ead75d0ddb2c695d84fb321
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-01-18 21:22:53 +0200

    more allocate

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,14 +14,18 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, len1, len2;
 	char *arr;
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+		;
+
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+		;
 
 	if (n >= len2)
 		n = len2;
