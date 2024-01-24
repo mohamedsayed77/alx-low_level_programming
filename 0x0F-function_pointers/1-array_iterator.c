a0b982a2fc0861c7380fa4032c976126ed4efdb1
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-01-24 23:36:33 +0200

    function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,7 +5,7 @@
 * on each element of an array
 * @array: pointer to array of integer
 * @size: size of  array
-* @action: pointer to function
+* @action: pointer to the function
 */
 
 void array_iterator(int *array, size_t size, void (*action)(int))
