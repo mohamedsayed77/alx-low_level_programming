4c18023e82a16547acd43d43a9be9ec021900fab
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-01-26 03:57:10 +0200

    function pointer

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,27 @@
-
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <string.h>
+
+/**
+* struct token - struct token
+* @token: format token
+* @f: the function associated
+*/
+
+
+typedef struct token
+{
+	char *token;
+
+	void (*f)(char *, va_list);
+} token_t;
+
+
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
