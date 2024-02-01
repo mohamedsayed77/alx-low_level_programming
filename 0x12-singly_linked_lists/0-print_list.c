fc7589b20b42a654f780917a301578a7441b163e
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-02-02 00:03:19 +0200

    linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+include "lists.h"
+
+/**
+* print_list - a functions that prints a linked lists
+* @h: pointer to the head
+* Return: the size of list
+*
+*/
+
+size_t print_list(const list_t *h)
+{
+
+	size_t i = 0;
+
+	while (h)
+	{
+
+		printf("[%d] %s\n", strlen(h->str),
+		h->str ? h->str : "(nil)");
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
