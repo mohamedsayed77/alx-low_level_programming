86ca7f9d4f0123a72a7b9ee9e958f700c3f20740
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-02-07 01:37:19 +0200

    more linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -12,29 +12,26 @@
 
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *newnode = malloc(sizeof(listint_t));
+	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *node;
 
-
-	if (n <= 0)
-	{
-		free(newnode);
+	if (!head || !new_node)
 		return (NULL);
 
-	}
-	newnode->n = n;
-	newnode->next = NULL;
+	new_node->next = NULL;
+	new_node->n = n;
 
 	if (!*head)
+		*head = new_node;
+
+	else
 	{
-		*head = newnode;
-		return (*head);
+		node = *head;
+
+		while (node->next)
+			node = node->next;
+
+		node->next = new_node;
 	}
-
-	listint_t *ptr = *head;
-
-	while (ptr->next != NULL)
-		ptr = ptr->next;
-
-	ptr->next = newnode;
-	return (*head);
+	return (new_node);
 }
