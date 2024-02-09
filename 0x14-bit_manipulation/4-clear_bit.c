50580b55be14b42ea63debf13e8a79f3a6fb8abe
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-02-10 01:18:52 +0200

    bitwise

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - a function that clears the bit at the index
+ * @n: takes the number to index
+ * @index: takes the bit to clear
+ * Return: 1 if successful, -1 if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	if (*n & 1L << index)
+		*n ^= 1L << index;
+	return (1);
+}
