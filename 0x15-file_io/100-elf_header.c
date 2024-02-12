1163cc18c16f45c43313c9cd53c197e73ee24246
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-02-13 01:15:38 +0200

    file i/o

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -2,24 +2,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <string.h>
 #include <elf.h>
+#include <string.h>
 
-void print_elf_header(const Elf64_Ehdr *header);
-int is_elf_file(const Elf64_Ehdr *header);
+void print_elf_header(Elf64_Ehdr *ehdr);
+int check_elf_file(int fd, Elf64_Ehdr *ehdr);
+void exit_with_error(int fd, const char *message);
 
 /**
-* main - Entry point, program that displays information contained in ELF header
+* main - Entry point, prints the ELF header of a file
 * @argc: Argument count
-* @argv: Argument vector
-*
-* Return: On success, EXIT_SUCCESS. On error, exits with status code 98.
+* @argv: Argument values
+* Return: EXIT_SUCCESS or EXIT_FAILURE
 */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int fd;
-	Elf64_Ehdr header;
+	Elf64_Ehdr ehdr;
 
 	if (argc != 2)
 	{
@@ -28,67 +28,75 @@ int main(int argc, char *argv[])
 	}
 
 	fd = open(argv[1], O_RDONLY);
-	if (fd < 0)
-	{
-		perror("Error opening file");
-		exit(98);
-	}
 
-	if (read(fd, &header, sizeof(header)) != sizeof(header))
-	{
-		fprintf(stderr, "Error reading ELF header\n");
-		close(fd);
-		exit(98);
-	}
+	if (fd == -1)
+		exit_with_error(fd, "Error opening file");
 
-	if (!is_elf_file(&header))
-	{
-		fprintf(stderr, "File is not a valid ELF file\n");
-		close(fd);
-		exit(98);
-	}
+	if (!check_elf_file(fd, &ehdr))
+		exit_with_error(fd, "File is not an ELF file");
 
-	print_elf_header(&header);
+	print_elf_header(&ehdr);
 	close(fd);
-	return (0);
+	return (EXIT_SUCCESS);
 }
 
 /**
-* is_elf_file - Checks if file is an ELF file
-* @header: Pointer to ELF header structure
-*
-* Return: 1 if file is ELF, otherwise 0
+* check_elf_file - Checks if the file is an ELF file by
+*	 reading the magic number
+* @fd: File descriptor of the file
+* @ehdr: Pointer to ELF header struct
+* Return: 1 if file is ELF, 0 otherwise
 */
 
-int is_elf_file(const Elf64_Ehdr *header)
+int check_elf_file(int fd, Elf64_Ehdr *ehdr)
 {
-	return (memcmp(header->e_ident, ELFMAG, SELFMAG) == 0);
+	if (read(fd, ehdr, sizeof(*ehdr)) != sizeof(*ehdr))
+		exit_with_error(fd, "Error reading ELF header");
+
+	if (memcmp(ehdr->e_ident, ELFMAG, SELFMAG) != 0)
+		return (0);
+
+	return (1);
 }
 
-
 /**
-* print_elf_header - Prints the ELF header information
-* @header: Pointer to ELF header
+* exit_with_error - Prints an error message and exits with status code 98
+* @fd: File descriptor to be closed if valid
+* @message: Error message to print to stderr
 */
 
-void print_elf_header(const Elf64_Ehdr *header)
+void exit_with_error(int fd, const char *message)
 {
-	printf("Magic:   ");
+	if (fd != -1)
+		close(fd);
+	fprintf(stderr, "%s\n", message);
+	exit(98);
+}
+
+/**
+* print_elf_header - Prints specific information from the ELF file header
+* @ehdr: Pointer to ELF header struct
+*/
+
+void print_elf_header(Elf64_Ehdr *ehdr)
+{
+	printf("ELF Header:\n");
+	printf("  Magic:   ");
 	for (int i = 0; i < EI_NIDENT; i++)
-		printf("%02x ", header->e_ident[i]);
+		printf("%02x ", ehdr->e_ident[i]);
 	printf("\n");
 
-	printf("Class:                             ELF%d\n",
-		header->e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
-	printf("Data:                              %s\n",
-		header->e_ident[EI_DATA] == ELFDATA2LSB ?
-		"2's complement, little endian" : "2's complement, big endian");
-	printf("Version:                           %u (current)\n",
-		header->e_ident[EI_VERSION]);
-	printf("OS/ABI:                            UNIX - System V\n");
-	printf("ABI Version:                       %u\n",
-		header->e_ident[EI_OSABI]);
-	printf("Type:                              %u\n", header->e_type);
-	printf("Entry point address:               0x%lx\n",
-		(unsigned long)header->e_entry);
+	printf("  Class:                             ELF%d\n",
+	ehdr->e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
+	printf("  Data:                              2's complement, %s\n",
+	ehdr->e_ident[EI_DATA] == ELFDATA2LSB ? "little endian" : "big endian");
+	printf("  Version:                           %u (current)\n",
+	ehdr->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            UNIX - System V\n");
+	printf("  ABI Version:                       %u\n",
+	ehdr->e_ident[EI_OSABI]);
+	printf("  Type:                              %u\n",
+	ehdr->e_type);
+	printf("  Entry point address:               0x%lx\n",
+	(unsigned long)ehdr->e_entry);
 }
