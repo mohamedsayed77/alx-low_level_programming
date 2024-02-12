93db46fe71f60ac0fe59cec7d4196d0266ab297a
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-02-13 01:02:46 +0200

    file i/o

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,13 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#define USAGE "Usage: cp file_from file_to\n"
+#define ERR_NOREAD "Error: Can't read from file %s\n"
+#define ERR_NOWRITE "Error: Can't write to %s\n"
+#define ERR_NOCLOSE "Error: Can't close fd %d\n"
+#define PERMISSIONS (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+
 #define READ_BUFFER_SIZE 1024
 
 ssize_t read_textfile(const char *filename, size_t letters);
