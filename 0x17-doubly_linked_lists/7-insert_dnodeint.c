f175235154f01a615cf2b1eab503efee9dd4a940
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-03-22 04:38:03 +0200

    doubly linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h, *new;
 
 	if (idx == 0)
-		return (add_dnodeint(h, n);
+		return (add_dnodeint(h, n));
 
 	for (; idx != 1; idx--)
 	{
