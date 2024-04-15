dc06c2486eaa5444f24b0bbe4a087ebe03708ec5
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-04-15 02:02:52 +0200

    hash_table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -14,14 +14,16 @@ void hash_table_print(const hash_table_t *ht);
 
 	if (ht == NULL || ht->array == NULL)
 		return;
+
 	printf("{");
-	for (i = 0; i < ht->size; ++i)
+	for (i = 0; i < ht->size; i++)
 	{
 		tmp = ht->array[i];
 		while (tmp != NULL)
 		{
 			if (flag == 1)
 				printf(", ");
+
 			printf("'%s': '%s'", tmp->key, tmp->value);
 			flag = 1;
 			tmp = tmp->next;
