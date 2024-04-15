2bf65e11c33819f11d27f92f37c7a5aa2d7b6fc0
mohamedsayed77 <mohamed.gemy0007@gmail.com>
2024-04-15 02:35:59 +0200

    hash_table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: hash table to delete
+ *
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int  i;
+	hash_node_t *next;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+
+	for (i = 0 ; i < ht->size ; ++i)
+	{
+		next = ht->array[i]->next;
+
+		free(ht->array[i]->key);
+		free(ht->array[i]->value);
+		free(ht->array[i]);
+		ht->array[i] = next;
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = NULL;
+	free(ht);
+}
